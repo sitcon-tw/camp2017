73287eac848a9deb0b85e8db94c44a53c03d76d7
aries0d0f <aries0d0f@gmail.com>
2017-05-05 16:08:54 +0800

    Fix RWD

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,21 @@
 var scrollHeight, distance
 const height = document.documentElement.clientHeight || document.body.clientHeight
-const pages = ['about', 'coc', 'coc1', 'schedule', 'schedule1', 'subject', 'active', 'experience']
+const pages = ['about', 'coc', 'schedule', 'subject', 'experience', 'footer']
 
 function bar() {
     scrollHeight = document.documentElement.scrollTop || document.body.scrollTop
-    distance = (scrollHeight - height) / height;
-    if (scrollHeight >= height) {
+    distance = (scrollHeight - height + 60) / height;
+    if (scrollHeight >= (height - 60)) {
         $('#bar').addClass('scrolled')
     } else {
         $('#bar').removeClass('scrolled')
     }
     for (var i = 0; i < pages.length; ++i) {
-        if (distance >= i && distance < (i + 1)) {
-            $("#bar").addClass(pages[i])
-            $('#'+pages[i]).addClass('animation')
+        console.log($('#' + pages[i]).offset().top, scrollHeight)
+        if((scrollHeight >= ($('#' + pages[i]).offset().top - 60)) && (scrollHeight < ($('#' + pages[i + 1]).offset().top - 57))) {
+            $('#bar').addClass(pages[i])
         } else {
-            $("#bar").removeClass(pages[i])
-            $('#'+pages[i]).removeClass('animation')
+            $('#bar').removeClass(pages[i])
         }
     }
 }
@@ -31,4 +30,9 @@ $(document).ready(function () {
             scrollTop: $($(this).attr('href')).offset().top
         }, 300);
     });
+    $('.toggle').click(function () {
+        $('.toggle').toggleClass('active')
+        $('#nav').toggleClass('active')
+        $('#nav>ul').toggleClass('active')
+    })
 })
