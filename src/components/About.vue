17602907e272d344021591bca8ddad4272ca36ef
Erickson Juang <a820224@gmail.com>
2017-05-20 03:27:36 +0800

    Implement component - about

---
+++ src/components/About.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="about" class="container">
+    <h1 class="title">關於 SITCON 夏令營</h1>
+    <div class="content">
+      <p>有鑑於資訊領域逐漸成為科技時代的重要學門，社群與開源精神亦持續抬頭，SITCON 團隊認為將資訊教育向下扎根，為在資訊科學領域有興趣的學生提供一個親切而良好的機會入門，並播灑開源種子，是一件相當重要且值得去做的事情。</p>
+      <p>2014年至今，SITCON 夏令營已將邁入第四屆，在此之前課程內容涵蓋開源碼、資訊安全、Maker、Python、後端的 Node.js、前端的資料爬蟲、視覺化等，更有社群闖關、視界咖啡館、黑客松等活動，讓學員充分吸收新知、尋找自己的愛好及興趣，進而加入我們一起推動Open Source。</p>
+      <p>2017 年度，以「踏入資訊域，放眼開源界」為主軸，SITCON 夏令營將於東海大學舉辦，承接開源鄉的概念，配合豐富的課程、座談會及社群闖關，透過程式編寫管理、演算法思維、團隊合作等架構下學習，讓學員推開程式大門、踏入資訊領域，並認識「開源」世界的廣大，透過開源城的引導，甚至做出自己的聊天機器人，擁有截然不同的營隊體驗。</p>
+    </div>
+    <h1 class="subtitle">＃ 適合參加族群</h1>
+    <div class="content">
+      <p>本次夏令營適合參加的族群為涉入資訊領域未深的學生，今年課程有別於往年，我們降低課程難度，採用適合新手的Python 程式語言，透過結合生活中時常接觸的通訊軟體，讓對資訊有興趣的學員，在參加完夏令營後可以學到基礎的程式設計，藉此打開通往資訊領域的大門。由於我們招收的族群主要是學生，故營期內也會邀請知名人士分享自身一路走來的故事，並讓學員有機會和這些平時較難接觸到的人近距離交談，期望學員在學習技術之餘，也能點燃指引未來的明燈。</p>
+    </div>
+    <div class="row">
+      <div class="items">
+        <h1 class="subtitle">＃ 活動時間</h1>
+        <p class="center">8/14（一）~ 8/17（四）</p>
+      </div>
+      <div class="items">
+        <h1 class="subtitle">＃ 活動地點</h1>
+        <p class="center">東海大學</p><p>台中市西屯區台灣大道四段1727號</p>
+      </div>
+    </div>
+    <h1 class="subtitle">＃ 共同主辦</h1>
+    <ul id="organizer">
+      <li>
+        <a href="http://sitcon.org/2017/" target="_blank">
+          <div class="img"><img src="/static/sitcon.png" alt=""></div>
+          <span>SITCON 學生計算機年會</span>
+        </a>
+      </li>
+      <li>
+        <a href="http://ocf.tw/" target="_blank">
+          <div class="img"><img src="/static/OCFLogo.png" alt="" /></div>
+          <span>開放文化基金會</span>
+        </a>
+      </li>
+      <li>
+        <a href="http://www.cs.thu.edu.tw/" target="_blank">
+          <div class="img"><img src="/static/cs-logo.jpg" alt="" /></div>
+          <span>東海大學 資訊工程學系</span>
+        </a>
+      </li>
+      <li>
+        <a href="http://itservice.thu.edu.tw/" target="_blank">
+          <div class="img"><img src="/static/itservice_logo.png" alt="" /></div>
+          <span>東海大學 電子計算機中心</span>
+        </a>
+      </li>
+    </ul>
+    <h1 class="title">關於 SITCON</h1>
+    <div class="content">
+      <p>2012 年中，一群學生在社群浪潮中為相同的理想聚集，秉持「以學生為本、由學生自發舉辦」的核心理念，SITCON 學生計算機年會及同名社群自 2013 年起，陸續舉行年會、定期聚、黑客松、工作坊、夏令營與多場演講，於技術交流之餘，亦在推廣資訊及開源教育上不遺餘力，希望透過技術與知識的激盪，給予學生們一個用自身力量實踐夢想的舞台。</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus">
+blue = #35b4b8
+
+#about
+  .title
+    background-color: blue
+
+  .items
+    text-align: center
+    margin: 0 3em
+    width: 300px
+
+#organizer
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  flex-wrap: wrap
+  font-weight: 200
+  width: 80%
+  margin: 0 auto
+
+  li
+    text-align: center
+    list-style: none
+
+    .img
+        margin: .5em auto
+        border: 2px solid white
+        border-radius: 5px
+        width: 240px
+        height: 160px
+        background-color: white
+        display: flex
+        align-items: center
+        justify-content: center
+        img
+          max-width: 80%
+          max-height: 80%
+
+    span
+      margin: 1em
+      font-weight: 400
+</style>
