9848aa9e6d86b415e92746ffbaa4bf213c9496fd
Erickson Juang <a820224@gmail.com>
2017-05-26 03:18:31 +0800

    Update course content

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -1,6 +1,217 @@
 <template>
   <section id="course" class="container">
     <h1 class="title">課程</h1>
+    <table id="timetable">
+      <thead>
+        <tr>
+          <td></td>
+          <td>Day 1</td>
+          <td>Day 2</td>
+          <td>Day 3</td>
+          <td>Day 4</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <p>07:00<br>~<br>07:30</p>
+          </td>
+          <td rowspan="2">
+            <p>傳送門已開啟</p>
+            <p class="sub">移動</p>
+          </td>
+          <td>
+            <p>迎接太陽之光</p>
+          </td>
+          <td>
+            <p>自我升級完成</p>
+          </td>
+          <td rowspan="2">
+            <p>養精蓄銳</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>07:30<br>~<br>09:00</p>
+          </td>
+          <td>
+            <p>神秘歷練</p>
+          </td>
+          <td>
+            <p>奇妙際遇</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>09:00<br>~<br>10:00</p>
+          </td>
+          <td>
+            <p>靈魂衝擊</p>
+            <p class="sub">開幕＆報到</p>
+          </td>
+          <td rowspan="4">
+            <p>記憶竄改</p>
+            <p class="sub">
+              【廣度課程】<br />HTTP、RESTful 觀念與實作
+            </p>
+          </td>
+          <td rowspan="4">
+            <p>傳承</p>
+            <p class="sub">
+              視界咖啡館
+            </p>
+          </td>
+          <td rowspan="2">
+            <p>最後衝刺</p>
+            <p class="sub">
+              黑客松
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>10:00<br>~<br>11:00</p>
+          </td>
+          <td rowspan="3">
+            <p>迎接新世界</p>
+            <p class="sub">破冰活動</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>11:00<br>~<br>11:30</p>
+          </td>
+          <td>
+            <p>機緣遇恩師</p>
+            <p class="sub">短講</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>11:30<br>~<br>12:00</p>
+          </td>
+          <td>
+            <p>回歸正途</p>
+            <p class="sub">短講</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>12:00<br>~<br>13:30</p>
+          </td>
+          <td colspan="4">
+            <p>暴風雨前的寧靜</p>
+            <p class="sub">午餐</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>13:30<br>~<br>15:30</p>
+          </td>
+          <td rowspan="2">
+            <p>魔法師之路</p>
+            <p class="sub">【基礎課程】<br />Python 3</p>
+          </td>
+          <td rowspan="2">
+            <p>高人相助</p>
+            <p class="sub">【基礎課程】<br />Python 3 × HTTP</p>
+          </td>
+          <td rowspan="2">
+            <p>餽贈</p>
+            <p class="sub">【基礎課程】<br />Telegram Chatbot</p>
+          </td>
+          <td>
+            <p>審判之時</p>
+            <p class="sub">黑客松發表</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>15:30<br>~<br>16:30</p>
+          </td>
+          <td rowspan="2">
+            <p>一切的真相</p>
+            <p class="sub">閉幕</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>16:30<br>~<br>17:00</p>
+          </td>
+          <td>
+            <p>尋找自我</p>
+          </td>
+          <td>
+            <p>整裝戰備</p>
+            <p class="sub">短講</p>
+          </td>
+          <td rowspan="2">
+            <p>臨門一腳</p>
+            <p class="sub">廣度課程</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>17:00<br>~<br>18:00</p>
+          </td>
+          <td>
+            <p>與市長的會面</p>
+            <p class="sub">晚餐</p>
+          </td>
+          <td rowspan="4">
+            <p>新血召募</p>
+            <p class="sub">晚餐＆社群闖關</p>
+          </td>
+          <td rowspan="5">
+            <p>崩壞之城</p>
+            <p class="sub">返家</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>18:00<br>~<br>19:00</p>
+          </td>
+          <td rowspan="3">
+            <p>初試身手</p>
+          </td>
+          <td>
+            <p>突破前兆</p>
+            <p class="sub">【廣度課程】</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>19:00<br>~<br>20:00</p>
+          </td>
+          <td>
+            <p>休戰</p>
+            <p class="sub">晚餐</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>20:00<br>~<br>21:00</p>
+          </td>
+          <td rowspan="2">
+            <p>城市的考驗</p>
+            <p class="sub">黑客松</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>21:00<br>~<br>22:00</p>
+          </td>
+          <td>
+            <p>盤點收穫</p>
+            <p class="sub">宵夜</p>
+          </td>
+          <td>
+            <p>揭發狐狸尾巴</p>
+            <p class="sub">神秘活動＆宵夜</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <h2 class="subtitle">廣度課程與短講</h2>
     <div class="content">
       <p>今年夏令營有別於往年的單一教學方式，而是在不影響一般課程的情況，安排一些廣度課程，讓學員在學習基礎課程之餘，也能學到資訊領域的其他層面，增加自身資訊技能的廣度。 除基礎課程與廣度課程外，我們也邀請到許多在資訊領域打滾多年的人士分享自身經驗，期許迷茫中的學員，可以點燃未來的一盞明燈，在資訊道路上有前進的動力及方向。</p>
@@ -18,218 +229,6 @@
       <p>黑客松（Hackathon）是「黑客」（Hack；原指早期為電腦系統找尋不完美的地方，或是探索不同的解法，來改善電腦系統）跟「馬拉松」（Marathon）的組合字，顧名思義，也就像是程式設計、工程界的馬拉松。由有興趣的一群人在有限的時間內，發揮自己的專長做出一個有特色的專案。</p>
       <p>今年， SITCON 夏令營將為學員打造一場專屬的小型黑客松，取代傳統的晚會、唱歌跳舞等活動，以小隊為單位，在吃吃喝喝的同時，一起發揮創意、運用課程中所學，做出與眾不同的作品呈現！</p>
     </div>
-
-    <h1 class="title">課表</h1>
-    <table id="timetable">
-      <thead>
-        <tr>
-          <td></td>
-          <td>Day 1</td>
-          <td>Day 2</td>
-          <td>Day 3</td>
-          <td>Day 4</td>
-        </tr>
-      </thead>
-      <tr>
-        <td>
-          <p>07:00<br>~<br>07:30</p>
-        </td>
-        <td rowspan="2">
-          <p>傳送門已開啟</p>
-          <p class="sub">移動</p>
-        </td>
-        <td>
-          <p>迎接太陽之光</p>
-        </td>
-        <td>
-          <p>自我升級完成</p>
-        </td>
-        <td rowspan="2">
-          <p>養精蓄銳</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>07:30<br>~<br>09:00</p>
-        </td>
-        <td>
-          <p>神秘歷練</p>
-        </td>
-        <td>
-          <p>奇妙際遇</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>09:00<br>~<br>10:00</p>
-        </td>
-        <td>
-          <p>靈魂衝擊</p>
-          <p class="sub">開幕＆報到</p>
-        </td>
-        <td rowspan="4">
-          <p>記憶竄改</p>
-          <p class="sub">
-            【廣度課程】<br />HTTP、RESTful 觀念與實作
-          </p>
-        </td>
-        <td rowspan="4">
-          <p>傳承</p>
-          <p class="sub">
-            視界咖啡館
-          </p>
-        </td>
-        <td rowspan="2">
-          <p>最後衝刺</p>
-          <p class="sub">
-            黑客松
-          </p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>10:00<br>~<br>11:00</p>
-        </td>
-        <td rowspan="3">
-          <p>迎接新世界</p>
-          <p class="sub">破冰活動</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>11:00<br>~<br>11:30</p>
-        </td>
-        <td>
-          <p>機緣遇恩師</p>
-          <p class="sub">短講</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>11:30<br>~<br>12:00</p>
-        </td>
-        <td>
-          <p>回歸正途</p>
-          <p class="sub">短講</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>12:00<br>~<br>13:30</p>
-        </td>
-        <td colspan="4">
-          <p>暴風雨前的寧靜</p>
-          <p class="sub">午餐</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>13:30<br>~<br>15:30</p>
-        </td>
-        <td rowspan="2">
-          <p>魔法師之路</p>
-          <p class="sub">【基礎課程】<br />Python 3</p>
-        </td>
-        <td rowspan="2">
-          <p>高人相助</p>
-          <p class="sub">【基礎課程】<br />Python 3 × HTTP</p>
-        </td>
-        <td rowspan="2">
-          <p>餽贈</p>
-          <p class="sub">【基礎課程】<br />Telegram Chatbot</p>
-        </td>
-        <td>
-          <p>審判之時</p>
-          <p class="sub">黑客松發表</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>15:30<br>~<br>16:30</p>
-        </td>
-        <td rowspan="2">
-          <p>一切的真相</p>
-          <p class="sub">閉幕</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>16:30<br>~<br>17:00</p>
-        </td>
-        <td>
-          <p>尋找自我</p>
-        </td>
-        <td>
-          <p>整裝戰備</p>
-          <p class="sub">短講</p>
-        </td>
-        <td rowspan="2">
-          <p>臨門一腳</p>
-          <p class="sub">廣度課程</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>17:00<br>~<br>18:00</p>
-        </td>
-        <td>
-          <p>與市長的會面</p>
-          <p class="sub">晚餐</p>
-        </td>
-        <td rowspan="4">
-          <p>新血召募</p>
-          <p class="sub">晚餐＆社群闖關</p>
-        </td>
-        <td rowspan="5">
-          <p>崩壞之城</p>
-          <p class="sub">返家</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>18:00<br>~<br>19:00</p>
-        </td>
-        <td rowspan="3">
-          <p>初試身手</p>
-        </td>
-        <td>
-          <p>突破前兆</p>
-          <p class="sub">【廣度課程】</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>19:00<br>~<br>20:00</p>
-        </td>
-        <td>
-          <p>休戰</p>
-          <p class="sub">晚餐</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>20:00<br>~<br>21:00</p>
-        </td>
-        <td rowspan="2">
-          <p>城市的考驗</p>
-          <p class="sub">黑客松</p>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <p>21:00<br>~<br>22:00</p>
-        </td>
-        <td>
-          <p>盤點收穫</p>
-          <p class="sub">宵夜</p>
-        </td>
-        <td>
-          <p>揭發狐狸尾巴</p>
-          <p class="sub">神秘活動＆宵夜</p>
-        </td>
-      </tr>
-      <tbody></tbody>
-    </table>
   </section>
 </template>
 
@@ -279,7 +278,6 @@ light-gray = #565964
 
   td
     height: 3em
-    font-weight: 800
     box-shadow: 0 0 0 1px white inset
     &[rowspan]
       background-color: red
